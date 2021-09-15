708f116a78ac4ddf67d39af8a3fba87a70e2b8f1
Leonardo <leocv0205@gmail.com>
2021-09-15 00:01:20 -0500

    Se crea el componente que creará peliculas haciendo conexión a la api

--- frontend/crud-peliculas-sofka/src/App.js
+++ frontend/crud-peliculas-sofka/src/App.js
@@ -1,8 +1,13 @@
+import React from 'react';
+
+import CreateMovie from './components/CreateMovie';
+
 
 function App() {
   return (
     <div className="App">
         <h1>Hola mundo</h1>
+        <CreateMovie/>
     </div>
   );
 }
